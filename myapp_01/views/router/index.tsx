d162c9101533db18139edde23cf6bde73c409130
bcygsws <bcygswspring168@gmail.com>
2025-04-25 23:38:38 +0800

    :memo: RN项目路由配置

---
+++ myapp_01/views/router/index.tsx
@@ -0,0 +1,10 @@
+/**
+ * @Desc：React Native的router配置
+ *
+ * 参考文档1：https://blog.csdn.net/Ght19970126/article/details/144259622
+ * RN路由配置包安装（6个）：
+ * npm install @react-navigation/native --save
+ * npm install react-native-animated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view --save
+ * @react-native-community/masked-view
+ *
+ * */
